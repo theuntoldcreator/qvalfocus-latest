8d2769a8cc700b85f2a16f426bb77ab6bd2f6468
[dyad] <saisatyapavanpanduvanka@my.unt.edu>
2025-08-09 15:42:46 -0500

    [dyad] Fixing server port issue - wrote 1 file(s)

--- server/index.ts
+++ server/index.ts
@@ -60,7 +60,15 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '3000', 10);
+  const portArgIndex = process.argv.indexOf('--port');
+  let port: number;
+
+  if (portArgIndex > -1 && process.argv[portArgIndex + 1]) {
+    port = parseInt(process.argv[portArgIndex + 1], 10);
+  } else {
+    port = parseInt(process.env.PORT || '5000', 10);
+  }
+
   server.listen({
     port,
     host: "0.0.0.0",
